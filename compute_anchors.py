6a681676d70d7a2b0ba4c8c650ca3e610204576a
yanglinzhuo <YangLinzhuo@users.noreply.github.com>
2023-06-07 14:20:48 +0800

    Autoanchor (#52)
    * Add autoanchor
    * Add separate autoanchor computation script
    
    ---------
    
    Co-authored-by: yanglinzhuo <yanglinzhuo@huawei.com>

---
+++ compute_anchors.py
@@ -0,0 +1,24 @@
+import argparse
+import ast
+
+from src.autoanchor import kmean_anchors
+
+def compute_anchors(args):
+    anchors = kmean_anchors(args.dataset, args.num_anchors, args.img_size, args.threshold,
+                            args.gen, args.verbose)
+    with open("anchors.txt", "w") as file:
+        file.write(str(anchors))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Compute anchors")
+    parser.add_argument("--dataset", type=str, default="./config/data/coco.yaml", help="Path to dataset config file.")
+    parser.add_argument("--num_anchors", type=int, default=9, help="The number of anchors.")
+    parser.add_argument("--img_size", type=int, default=640, help="Image size.")
+    parser.add_argument("--threshold", type=float, default=4.0,
+                        help="Anchor-label wh ratio threshold hyperparameter hyp['anchor_t'] used for training, default=4.0")
+    parser.add_argument("--gen", type=int, default=1000,
+                        help="The number of generations to evolve anchors using genetic algorithm")
+    parser.add_argument("--verbose", type=ast.literal_eval, default=True, help="Whether print verbose information.")
+    args = parser.parse_args()
+    compute_anchors(args)
--- src/autoanchor.py
+++ src/autoanchor.py
@@ -15,7 +15,16 @@
 
 # Auto-anchor utils
 
+import copy
+import random
+
+import numpy as np
+from tqdm import tqdm
+import yaml
+import mindspore as ms
 from mindspore import ops
+
+from src.general import LOGGER
 
 
 def check_anchor_order(m):
@@ -24,6 +33,135 @@ def check_anchor_order(m):
     da = a[-1] - a[0]  # delta a
     ds = m.stride[-1] - m.stride[0]  # delta s
     if ops.Sign()(da) != ops.Sign()(ds): # same order
-        print('Reversing anchor order')
+        LOGGER.info('Reversing anchor order')
         m.anchors[:] = ops.ReverseV2(axis=0)(m.anchors)
         m.anchor_grid[:] = ops.ReverseV2(axis=0)(m.anchor_grid)
+
+
+def check_anchors(dataset, model, thr=4.0, imgsz=640):
+    # Check anchor fit to data, recompute if necessary
+    m = model.module.model[-1] if hasattr(model, 'module') else model.model[-1]  # Detect()
+    shapes = imgsz * dataset.shapes / dataset.shapes.max(1, keepdims=True)
+    scale = np.random.uniform(0.9, 1.1, size=(shapes.shape[0], 1))  # augment scale
+    wh = np.concatenate([l[:, 3:5] * s for s, l in zip(shapes * scale, dataset.labels)], dtype=np.float32)  # wh
+
+    def metric(k):  # compute metric
+        r = wh[:, None] / k[None]
+        x = np.minimum(r, 1 / r).min(2)  # ratio metric
+        best = x.max(1)[0]  # best_x
+        aat = (x > 1 / thr).sum(1).mean()  # anchors above threshold
+        bpr = (best > 1 / thr).mean()  # best possible recall
+        return bpr, aat
+
+    stride = m.stride.asnumpy().reshape(-1, 1, 1)  # model strides
+    anchors = copy.deepcopy(m.anchors).asnumpy()
+    anchors = anchors * stride  # current anchors
+    bpr, aat = metric(anchors.reshape(-1, 2))
+    s = f'\n{aat:.2f} anchors/target, {bpr:.3f} Best Possible Recall (BPR). '
+    if bpr > 0.98:  # threshold to recompute
+        LOGGER.info(f'{s}Current anchors are a good fit to dataset ✅')
+    else:
+        LOGGER.info(f'{s}Anchors are a poor fit to dataset ⚠️, attempting to improve...')
+        na = m.anchors.numel() // 2  # number of anchors
+        anchors = kmean_anchors(dataset, n=na, img_size=imgsz, thr=thr, gen=1000, verbose=False)
+        new_bpr = metric(anchors)[0]
+        if new_bpr > bpr:  # replace anchors
+            m.anchors[:] = ms.Tensor(anchors.reshape(m.anchors.shape), dtype=ms.float32)
+            check_anchor_order(m)  # must be in pixel-space (not grid-space)
+            m.anchors /= stride
+            s = f'Done ✅ (optional: update model *.yaml to use these anchors in the future)'
+        else:
+            s = f'Done ⚠️ (original anchors better than new anchors, proceeding with original anchors)'
+        LOGGER.info(s)
+
+
+def kmean_anchors(dataset='./data/coco128.yaml', n=9, img_size=640, thr=4.0, gen=1000, verbose=True):
+    """ Creates kmeans-evolved anchors from training dataset
+
+        Arguments:
+            dataset: path to data.yaml, or a loaded dataset
+            n: number of anchors
+            img_size: image size used for training
+            thr: anchor-label wh ratio threshold hyperparameter hyp['anchor_t'] used for training, default=4.0
+            gen: generations to evolve anchors using genetic algorithm
+            verbose: print all results
+
+        Return:
+            k: kmeans evolved anchors
+
+        Usage:
+            from utils.autoanchor import *; _ = kmean_anchors()
+    """
+    from scipy.cluster.vq import kmeans
+
+    npr = np.random
+    thr = 1 / thr
+
+    def metric(k, wh):  # compute metrics
+        r = wh[:, None] / k[None]
+        x = np.minimum(r, 1 / r).min(2)  # ratio metric
+        # x = wh_iou(wh, torch.tensor(k))  # iou metric
+        return x, x.max(1)[0]  # x, best_x
+
+    def anchor_fitness(k):  # mutation fitness
+        _, best = metric(k, wh)
+        return (best * (best > thr)).mean()  # fitness
+
+    def print_results(k, verbose=True):
+        k = k[np.argsort(k.prod(1))]  # sort small to large
+        x, best = metric(k, wh0)
+        bpr, aat = (best > thr).mean(), (x > thr).mean() * n  # best possible recall, anch > thr
+        s = f'thr={thr:.2f}: {bpr:.4f} best possible recall, {aat:.2f} anchors past thr\n' \
+            f'n={n}, img_size={img_size}, metric_all={x.mean():.3f}/{best.mean():.3f}-mean/best, ' \
+            f'past_thr={x[x > thr].mean():.3f}-mean: '
+        for x in k:
+            s += '%i,%i, ' % (round(x[0]), round(x[1]))
+        if verbose:
+            LOGGER.info(s[:-2])
+        return k
+
+    if isinstance(dataset, str):  # *.yaml file
+        with open(dataset, errors='ignore') as f:
+            data_dict = yaml.safe_load(f)  # model dict
+        from src.dataset import LoadImagesAndLabels
+        dataset = LoadImagesAndLabels(data_dict['train'], augment=True, rect=True)
+
+    # Get label wh
+    shapes = img_size * dataset.shapes / dataset.shapes.max(1, keepdims=True)
+    wh0 = np.concatenate([l[:, 3:5] * s for s, l in zip(shapes, dataset.labels)])  # wh
+
+    # Filter
+    i = (wh0 < 3.0).any(1).sum()
+    if i:
+        LOGGER.info(f'WARNING ⚠️ Extremely small objects found: {i} of {len(wh0)} labels are <3 pixels in size')
+    wh = wh0[(wh0 >= 2.0).any(1)].astype(np.float32)  # filter > 2 pixels
+    # wh = wh * (npr.rand(wh.shape[0], 1) * 0.9 + 0.1)  # multiply by random scale 0-1
+
+    # Kmeans init
+    try:
+        LOGGER.info(f'Running kmeans for {n} anchors on {len(wh)} points...')
+        assert n <= len(wh)  # apply overdetermined constraint
+        s = wh.std(0)  # sigmas for whitening
+        k = kmeans(wh / s, n, iter=30)[0] * s  # points
+        assert n == len(k)  # kmeans may return fewer points than requested if wh is insufficient or too similar
+    except Exception:
+        LOGGER.warning(f'WARNING ⚠️ switching strategies from kmeans to random init')
+        k = np.sort(npr.rand(n * 2)).reshape(n, 2) * img_size  # random init
+    k = print_results(k, verbose=False)
+
+    # Evolve
+    f, sh, mp, s = anchor_fitness(k), k.shape, 0.9, 0.1  # fitness, generations, mutation prob, sigma
+    pbar = tqdm(range(gen))  # progress bar
+    for _ in pbar:
+        v = np.ones(sh)
+        while (v == 1).all():  # mutate until a change occurs (prevent duplicates)
+            v = ((npr.random(sh) < mp) * random.random() * npr.randn(*sh) * s + 1).clip(0.3, 3.0)
+        kg = (k.copy() * v).clip(min=2.0)
+        fg = anchor_fitness(kg)
+        if fg > f:
+            f, k = fg, kg.copy()
+            pbar.desc = f'Evolving anchors with Genetic Algorithm: fitness = {f:.4f}'
+            if verbose:
+                print_results(k, verbose)
+
+    return print_results(k).astype(np.float32)
